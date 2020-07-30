93dc3aa87cf6c3650da5bd604e3232c60122b189
meslin8752 <alexandre@meslin.com.br>
2020-07-29 23:08:18 -0300

    First commit

---
+++ MeuApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    # processamento antes de mostrar a home page
+    return render(request, 'MeuApp/home.html')
+
+def segundaPagina(request):
+    # processamento antes de mostrar a segunda página
+    return render(request, 'MeuApp/segunda.html')
